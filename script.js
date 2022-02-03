eed7a0d626f8f7bcde172fbecfb570d7ca31fa7c
enzom07 <enzo.maragliano@gmail.com>
2022-02-03 09:46:25 -0300

    Edit index.html

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const container = document.querySelector('#container');
 
 for(i=0;i<16*16;i++){
     var div = document.createElement('div');
-    div.style.cssText = 'background: blue; height: 50px; width: 50px; margin:10px'; 
-    //content.classList.add('content');
-    //content.textContent = 'This is the glorious text-content!';
-
-container.appendChild(div);
-
+    div.classList.add('grid-item');  
+    container.appendChild(div);
+   
+   
 }
+//Select all squares created for the grid
+const squares = document.querySelectorAll(".grid-item");
+
+ for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseout", function() {
+        squares[i].classList.remove("red");
+     }); 
+     squares[i].addEventListener("mouseover", function() {
+        squares[i].classList.add("red");
+      });
+     
+ }
+   
+    
