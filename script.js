114b381c42a0c8fb96fb0bfc60f019937636dca7
enzom07 <enzo.maragliano@gmail.com>
2022-02-03 11:54:45 -0300

    Edit index.html

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 //Select Container
 const container = document.querySelector('#container');
-
+var div;
+var divStyle;
+var value;
 //Create the grid by default with each class square called grid-item
 for(let i=0;i<16*16;i++){
-    var div = document.createElement('div');
-    div.classList.add('grid-item');  
+    div = document.createElement('div');
+    div.classList.add('grid-item'+[i]); 
     container.appendChild(div);
+    divStyle = document.querySelector('.grid-item'+[i]).style;
+    divStyle.backgroundColor = 'blue';
+    divStyle.width = '50px';
+    divStyle.height = '50px'; 
+    divStyle.transition ='.5s all';
 }
 
 //Select all squares created for the grid
-var squares = document.querySelectorAll(".grid-item");
+var squares = document.querySelectorAll(".grid-container > div");
 
 //Houvering effect to each square
  for (let i = 0; i < squares.length; i++) {
+    value = 0.1;
     squares[i].addEventListener("mouseout", function() {
-        squares[i].classList.remove("red");
+        //squares[i].classList.remove("red");
+        divStyle = document.querySelector('.grid-item'+[i]).style;
+        divStyle.opacity = value;
+        divStyle.backgroundColor = 'red';
+        value = value + 0.1;
+        //divStyle.backgroundColor = random_rgba();
+        
      }); 
+     value = 0.1;
      squares[i].addEventListener("mouseover", function() {
-        squares[i].classList.add("red");
+        //squares[i].classList.add("red");
+        divStyle = document.querySelector('.grid-item'+[i]).style;
+        divStyle.opacity = value;
+        divStyle.backgroundColor = 'red';
+        value = value + 0.1;
+        //divStyle.backgroundColor = random_rgba();
       });
      
  }
@@ -36,22 +56,45 @@ var squares = document.querySelectorAll(".grid-item");
     for(let i=0; i < squares.length; i++){
         container.removeChild(squares[i]);
     }
+    //Create the new grid
     for(let i=0;i<input*input;i++){
         div = document.createElement('div');
-        div.classList.add('grid-item');  
+        div.classList.add('grid-item'+[i]); 
         container.appendChild(div);
+        divStyle = document.querySelector('.grid-item'+[i]).style;
+        divStyle.backgroundColor = 'blue';
+        divStyle.width = '50px';
+        divStyle.height = '50px'; 
+        divStyle.transition ='.5s all';
     } 
     //Select all squares created for the grid
-    squares = document.querySelectorAll(".grid-item");
+    squares = document.querySelectorAll(".grid-container > div");
 
     //Houvering effect to each square
     for (let i = 0; i < squares.length; i++) {
-      squares[i].addEventListener("mouseout", function() {
-        squares[i].classList.remove("red");
-     }); 
-      squares[i].addEventListener("mouseover", function() {
-        squares[i].classList.add("red");
-      });  
- }
+        squares[i].addEventListener("mouseout", function() {
+            //squares[i].classList.remove("red");
+            divStyle = document.querySelector('.grid-item'+[i]).style;
+            divStyle.backgroundColor = random_rgba();
+         }); 
+         squares[i].addEventListener("mouseover", function() {
+            //squares[i].classList.add("red");
+            divStyle = document.querySelector('.grid-item'+[i]).style;
+            divStyle.backgroundColor = random_rgba();
+          });
+         
+     }
 
   });
+
+
+  function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+function create_grid(){
+
+
+
+}
