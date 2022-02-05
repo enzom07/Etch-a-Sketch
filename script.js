208075109dc54ac18064c546d0f721c1f9b04d98
enzom07 <enzo.maragliano@gmail.com>
2022-02-05 18:08:01 -0300

    Edit index.html

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-//Select Container div
-const container = document.querySelector('#container');
+const def = 16;
+const size = 600;
+const btn = document.querySelector('#btn');
+const btn_clear = document.querySelector('#clear');
+const btn_eraser = document.querySelector('#eraser');
+const btn_scale = document.querySelector('#opacity');
+const btn_rainbow = document.querySelector('#rainbow');
+const cred = "#ff0000";
+const cwhite = "#ffffff";
+
+var square_size;
+var container = document.querySelector('#container');
 var div;
 var divStyle;
-const def = 16;
-//Create const btn to select the button in the container div
-const btn = document.querySelector('#btn');
+var input = def;
+var opa = 1; //Opacity
+var squares; 
 
 
-//Call function create_grid with the default value 16x16
-create_grid(def);
-//Select all squares created for the grid
-var squares = document.querySelectorAll(".grid-container > div");
-color();
- //For each click on the button
- btn.addEventListener('click', () => {
-    let input = prompt("Enter the number of squares per side for the new grid (<=100):");
+//For each click on the button
+btn.addEventListener('click', () => {
+    input = prompt("Enter the number of squares per side for the new grid (<=100):");
     while(input>100 || input <0){
         input = prompt("Invalid number...enter the number of squares per side for the new grid (x<=100 and x>0):");
     }
-    //Delete the current grid
-    delete_grid();
     //Create the new grid
     create_grid(input);
-    squares = document.querySelectorAll(".grid-container > div");
-    //Houvering effect to each square
-    houver_effect();
+    color(cred,1)
+});
+
+//Clear ALl
+  btn_clear.addEventListener('click', () => {
+    create_grid(input);
+    color(cred,opa);
   });
 
+//Eraser
+btn_eraser.addEventListener('click', () => {
+    color(cwhite);
+  });
+
+  //Color Scale
+btn_scale.addEventListener('click', () => {
+    color(cred,0.1);
+  });
+
+    //Rainbow
+btn_rainbow.addEventListener('click', () => {
+    color("rainbow",1);
+  });
 //Function random to the colors
   function random_rgba() {
     var o = Math.round, r = Math.random, s = 255;
@@ -35,50 +56,48 @@ color();
 
 //Create the grid by default with each class square called grid-item
 function create_grid(val){
+
     for(let i=0;i<val*val;i++){
+        square_size = size / val;
         div = document.createElement('div');
         div.classList.add('grid-item'+[i]); 
         container.appendChild(div);
         divStyle = document.querySelector('.grid-item'+[i]).style;
-        divStyle.backgroundColor = 'blue';
-        divStyle.width = '50px';
-        divStyle.height = '50px'; 
+        divStyle.backgroundColor = 'white';
+        divStyle.width = square_size+'px';
+        divStyle.height = square_size+'px';
+        //divStyle.border = 'solid'; 
         divStyle.transition ='.5s all';
     }
 }
 
 //Function to add color to squares from 10% to 100% of color
-function color(){   
-    //For ecah square
+function color(color,opa){   
+    squares = document.querySelectorAll(".grid-container > div");
     squares.forEach((div) => {
-        let value = 0.1;
-        //console.log(div);
+        let value = 1;
+        if(opa<1){
+            value = 0.1;
+        }
         div.addEventListener("mouseover",function(){
-            div.style.cssText = "background-color: red; width: 50px; height: 50px; transition: .5s all; opacity:"+value+"";
-            value= value + 0.1; 
+            if(color === "rainbow"){
+                div.style.cssText = "background-color:"+random_rgba()+"; width:"+square_size+"px; height:"+square_size+"px; transition: .5s all; opacity:"+value+"";
+            }
+            else{
+                div.style.cssText = "background-color:"+color+"; width:"+square_size+"px; height:"+square_size+"px; transition: .5s all; opacity:"+value+"";
+                if(value<1){
+                    value = value + 0.1; 
+                }
+             }
         });            
     }); 
-    
   }
 
-function delete_grid(){
-    for(let i=0; i < squares.length; i++){
-        container.removeChild(squares[i]);
-    }
+window.onload = () => {
+    //Grid default value 16x16 red color
+create_grid(input);
+color(cred,opa);
 }
 
-function houver_effect(){
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("mouseout", function() {
-            //squares[i].classList.remove("red");
-            divStyle = document.querySelector('.grid-item'+[i]).style;
-            divStyle.backgroundColor = random_rgba();
-         }); 
-         squares[i].addEventListener("mouseover", function() {
-            //squares[i].classList.add("red");
-            divStyle = document.querySelector('.grid-item'+[i]).style;
-            divStyle.backgroundColor = random_rgba();
-          });
-     }
-}
+
 
